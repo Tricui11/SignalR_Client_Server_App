d460be1695c3c6295358531b91523a1a91cefe07
dgutcko <dgutcko@WS-HUTSKO>
2022-03-29 09:14:04 +0300

    small improves

--- SignalRChat/Hubs/UserHandler.cs
+++ SignalRChat/Hubs/UserHandler.cs
@@ -7,15 +7,7 @@ namespace SignalRChat.Hubs
     public static class UserHandler
     {
         public static ICollection<ChatUser> ConnectedUsers = new List<ChatUser>();
-        public static ChatUser FindByConnectionId(string connectionId) => UserHandler.ConnectedUsers.First(x => x.ConnectionId == connectionId);
-        public static bool CanAddUserName(string userName)
-        {
-            IEnumerable<string> connectedUsersNames = UserHandler.ConnectedUsers.Select(x => x.Name);
-            if (connectedUsersNames.Contains(userName))
-            {
-                return false;
-            }
-            return true;
-        }
+        public static ChatUser FindByConnectionId(string connectionId) => ConnectedUsers.First(x => x.ConnectionId == connectionId);
+        public static bool CanAddUserName(string userName) => !ConnectedUsers.Select(x => x.Name).Contains(userName);
     }
 }
